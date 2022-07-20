7bce447cc57ec28115bd478f6af6dd0553d9c36f
tony-burns <74726011+tony-burns@users.noreply.github.com>
2022-07-20 14:15:05 -0400

    m1

---
+++ src/pages/_document.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          {/* LOAD FAVIICON AND RENDER GLOBAL CSSS */}
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
